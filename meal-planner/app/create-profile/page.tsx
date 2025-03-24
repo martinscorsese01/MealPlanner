07e8f3685c75c4a9e6f4471e895fbe3a8d8e918a
martinscorsese01 <117444599+martinscorsese01@users.noreply.github.com>
2025-03-24 16:34:37 +0000

    added the create profile functionality

--- meal-planner/app/create-profile/page.tsx
+++ meal-planner/app/create-profile/page.tsx
@@ -1,5 +1,23 @@
+'use client';
 
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 
 export default function CreateProfile() {
-    return <div> Processing sign in...</div>;  
-    }
+  const router = useRouter();
+
+  useEffect(() => {
+    const create = async () => {
+      await fetch('/api/create-profile', { method: 'POST' });
+      router.push('/subscribe'); // after creating profile
+    };
+
+    create();
+  }, [router]);
+
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <p>Creating your profile...</p>
+    </div>
+  );
+}
