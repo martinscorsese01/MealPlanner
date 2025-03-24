e5ce52a4135772c66f4129dd44cab6f2b598a9dc
martinscorsese01 <117444599+martinscorsese01@users.noreply.github.com>
2025-03-24 16:18:02 +0000

    AI meals description

--- meal-planner/app/sign-up/[[...sign-up]]/page.tsx
+++ meal-planner/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,9 +1,9 @@
-import { SignUp } from '@clerk/nextjs'
+import { SignUp } from '@clerk/nextjs';
 
 export default function SignUpPage() {
   return (
-    <div className="px-4 py-8 sm:py-12 lg:py-16 max-w-7xl mx-auto flex justify-center align-center">
-      <SignUp forceRedirectUrl="/subscribe" />{" "}
+    <div className="centered-container">
+      <SignUp />
     </div>
   );
-  }
+}
