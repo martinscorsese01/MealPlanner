d8a1411b7edb7a146d764d146ad6c41b2f27521c
martinscorsese01 <117444599+martinscorsese01@users.noreply.github.com>
2025-03-24 02:32:29 +0000

    middleware sign up route

--- meal-planner/app/layout.tsx
+++ meal-planner/app/layout.tsx
@@ -31,7 +31,9 @@ export default function RootLayout({
           className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-50 text-gray-900`}
         >
           <NavBar />
+          <div>
           {children}
+          </div> 
         </body>
       </html>
     </ClerkProvider>
