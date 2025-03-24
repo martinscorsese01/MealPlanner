c87cfd86de844df202f0dbd34631effc02611f38
martinscorsese01 <117444599+martinscorsese01@users.noreply.github.com>
2025-03-24 00:59:37 +0000

    add logo

--- meal-planner/components/navbar.tsx
+++ meal-planner/components/navbar.tsx
@@ -1,3 +1,16 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function NavBar() {
-    return <nav> Navbar </nav>
-    }
+  return (
+    <nav>
+      {" "}
+      <div>
+        <Link href="/">
+          <Image src="/logo.png" width={60} height={60} alt="Logo" />
+        </Link>
+      </div>
+      <div></div>
+    </nav>
+  );
+}
